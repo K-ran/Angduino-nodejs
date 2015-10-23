fb1c8c7d767f1b76f92607d13f6d8f1b8acf80d5
Karan Purohit <karnpurohit@gmail.com>
2015-10-23 19:19:59 +0530

    added two way binding

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ var sleep = require('sleep');
 var express = require('express');
 var app=express();
 var io = require('socket.io').listen(app.listen(3000,serverOn));
-var serialPort = new SerialPort("/dev/ttyACM0");
+
+var portName="/dev/ttyACM0";
+
+process.argv.forEach(function (val, index, array) {
+  if(index==2)
+  portName=val;
+});
+var serialPort = new SerialPort(portName,{parser: require("serialport").parsers.readline("\n")});
 
 app.use(express.static(__dirname + '/site/'));
 app.get('/', function(req, res){
@@ -28,19 +35,29 @@ function serverOn(){
 };
 
 
+//open the serial port
 serialPort.open(function (error) {
   if ( error ) {
-    console.log('failed to open: '+error);
+    console.log('failed to connect to Arduino on: '+portName+ '\n CHECK the port name again');
   } else {
-    sleep.sleep(1);
-    console.log('open');
+    sleep.sleep(1);   //wait for the arduino to properly start
+    console.log('Arduino connected successfully');
+
     io.on('connection', function(socket){
-      console.log('User Connected');
-      socket.on('button', function(msg){
+        var address = socket.handshake.address;
+        console.log('New connection from ' + socket.request.socket.remoteAddress);
+
+        //called when user clicks on any button on webpage
+        socket.on('button', function(msg){
+          //sends it to the arduino
           serialPort.write(msg+"\n", function(err, results) {
             console.log('error :' + err);
           });
       });
+      //when reading data from the arduino
+      serialPort.on('data', function(data) {
+                io.emit('angduino-data',data);
+        });
     });
   }
 });
--- site/angduino.js
+++ site/angduino.js
@@ -1,5 +1,5 @@
 var angduino = angular.module('angduino',[]);
-angduino.controller('appController',function($scope){
+angduino.controller('appController',function($scope,socket){
 });
 
 angduino.factory('socket',function(){
@@ -8,19 +8,32 @@ angduino.factory('socket',function(){
     return socket;
 });
 
-angduino.directive('angduinoButton',function(socket){
+angduino.directive('angduinoButton',function(socket,$parse){
     return {
         restrict:'A',
         link: function(scope, element, attrs, ctrl){
+            var model = $parse(attrs.angduinoButton);
+            // Assigns a value to it
+            model.assign(scope,1);
+            // Apply it to the scope
             var html = element.html();
             element.html(attrs.on);
+            socket.on('angduino-data', function(msg){
+                var dataObject=JSON.parse(msg);
+                if(typeof dataObject.button != "undefined"){
+                    eval(dataObject.button);
+                    if(scope[attrs.angduinoButton]==1)
+                        element.html(attrs.off);
+                    else element.html(attrs.on);
+                    console.log("Got the packeage");
+                }
+                //console.log(msg);
+            });
+
             element.on("click",function(){
                         var html2 = element.html();
+                        socket.emit("button","button");
                         socket.emit("button",attrs.angduinoButton);
-                        if(html2==attrs.off)
-                            element.html(attrs.on);
-                        else
-                            element.html(attrs.off);
                      });
         }
     }
